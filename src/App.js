eab18dcaae15ff4b8a16faf1b3a560ea2ed8597f
DmitryKisel85 <dim03@mail.ru>
2022-05-25 14:22:04 +0300

    Add Header component and styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import MainBlock from "./components/main-block/main-block.component";
 
 const App = () => {
 	return (
 		<div className='App'>
-			<header className='header'>
-				<h1>THINGS TO DO</h1>
-			</header>
+			<Header />
 			<MainBlock />
 		</div>
 	);
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import "./header.styles.css";
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<h1>THINGS TO DO</h1>
+		</header>
+	);
+};
+
+export default Header;
