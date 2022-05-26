3560e0d22adabfaa84dcfa2e8840d454eb56be53
DmitryKisel85 <dim03@mail.ru>
2022-05-26 19:33:48 +0300

    Refactor btn-check and btn-edit components markup and styles

--- src/components/btn-check/btn-check.component.jsx
+++ src/components/btn-check/btn-check.component.jsx
@@ -2,7 +2,7 @@ import "./btn-check.styles.css";
 
 const BtnCheck = ({ modifyEditing }) => {
 	return (
-		<button className='btn-edit' id='btn-edit' data-state='closed' onClick={modifyEditing}>
+		<button className='btn-edit' onClick={modifyEditing}>
 			<i className='fas fa-check'></i>
 		</button>
 	);
--- src/components/btn-edit/btn-edit.component.jsx
+++ src/components/btn-edit/btn-edit.component.jsx
@@ -2,7 +2,7 @@ import "./btn-edit.styles.css";
 
 const BtnEdit = ({ modifyEditing }) => {
 	return (
-		<button className='btn-edit' id='btn-edit' data-state='closed' onClick={modifyEditing}>
+		<button className='btn-edit' onClick={modifyEditing}>
 			<i className='far fa-edit'></i>
 		</button>
 	);
