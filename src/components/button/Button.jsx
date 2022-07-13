85b6cf8cfd28256911a0d516c574e999f545a950
DmitryKisel85 <dim03@mail.ru>
2022-07-13 21:13:02 +0300

    Refactor Button component to take correct props and add PropTypes checking for it

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,19 @@
+import PropTypes from "prop-types";
+
 import "./button.scss";
 
-const Button = ({ children, ...otherProps }) => {
-	return <button {...otherProps}>{children}</button>;
+const Button = ({ children, onClick, className }) => {
+	return (
+		<button onClick={onClick} className={className}>
+			{children}
+		</button>
+	);
+};
+
+Button.propTypes = {
+	children: PropTypes.node,
+	onClick: PropTypes.func,
+	className: PropTypes.string,
 };
 
 export default Button;
